4d0ccea19ed260c4135abddc88a812097c52437c
= <khadraniarif@gmail.com>
2025-05-03 00:55:30 +0530

    meals and commnity pages

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,24 @@
+import MealsGrid from '@/components/meals/meals-grid';
+import classes from './page.module.css';
+import Link from 'next/link';
+
 export default function Meals() {
-    return <div>Meals</div>
+    return <>
+        <header className={classes.header}></header>
+        <h1>
+            Delicious meals, created{' '}
+            <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+            Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+            <Link href="/meals/share">
+                Share your reciepe
+            </Link>
+        </p>
+        <main className={classes.main}>
+            <MealsGrid meals={[]} />
+        </main>
+    </>
 }
--- components/main-header.js
+++ components/main-header.js
@@ -1,20 +1,26 @@
+'use client';
+
 import Link from "next/link";
 
 import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function MainHeader() {
-    return <header>
-        <Link href="/">
-            <img src={logoImg.src} alt="A plate with food on it" />
+    const path = usePathname();
+    return <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+            <Image src={logoImg} alt="A plate with food on it" />
         </Link>
 
-        <nav>
+        <nav className={classes.nav}>
             <ul>
                 <li>
-                    <Link href='/meals'>Browse Meals</Link>
+                    <Link href='/meals' className={path.startsWith('/meals') ? classes.active : undefined}>Browse Meals</Link>
                 </li>
                 <li>
-                    <Link href='/community'>Foodies Community</Link>
+                    <Link href='/community' className={path.startsWith('/community') ? classes.active : undefined}>Foodies Community</Link>
                 </li>
             </ul>
         </nav>
